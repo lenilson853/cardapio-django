42d1da3e2e9e01c7293e9e8ff05ee2ce7799d8e7
lenilson jose <lenylson.nascimento@gmail.com>
2025-11-01 08:17:17 -0300

    Corrige rota do admin que estava faltando no urls.py

--- deposito_bebidas/urls.py
+++ deposito_bebidas/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path
-from cardapio.views import lista_bebidas  # <--- IMPORTE A SUA VIEW
+
+# 1. Importe a sua view do cardápio (como já tínhamos feito)
+from cardapio.views import lista_bebidas
 
 urlpatterns = [
+    # 2. ESTA É A LINHA QUE ESTAVA FALTANDO!
     path('admin/', admin.site.urls),
-    path('', lista_bebidas, name='home'), # <--- ADICIONE ESTA LINHA
+    
+    # 3. Esta é a linha da sua página principal (o cardápio)
+    path('', lista_bebidas, name='home'),
 ]
